005a25f355827d995dc3c58089d001df132e4615
bptedythe <bptedythe@gmx.com>
2025-03-05 22:06:00 +0000

    Add 2.rs

---
+++ src/2.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let mut rng = rand::thread_rng();
+    let number1 = rng.gen_range(1..=10);
+    let number2 = rng.gen_range(1..=10);
+
+    println!("What is {} + {}?", number1, number2);
+}
